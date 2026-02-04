d1fbdc8ff2a82512ef222f32b45ad26fd63f1561
Sofia Rest <68917129+srest2021@users.noreply.github.com>
2026-02-03 16:50:02 -0800

    Update example division calls in my_first_program.py

--- my_first_program.py
+++ my_first_program.py
@@ -10,4 +10,8 @@ def function_3(numerator, denominator):
 
 if __name__ == "__main__":
     print("This script contains code examples.")
-    print("Example division: ", function_3(10, 0))
+    print("Example division: ", function_3(10, 5))
+    print("Example division: ", function_3(20, 10))
+    print("Example division: ", function_3(10, 2))
+    print("Example division: ", function_3(20, 0))
+    print("Example division: ", function_3(0, 10))
