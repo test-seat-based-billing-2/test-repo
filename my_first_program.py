746e68c4301d0ac2b0a3fee2a0e40d5c7d6e831f
srest2021 <sofia.rest@sentry.io>
2026-02-03 16:46:06 -0800

    my first commit

---
+++ my_first_program.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python script with code examples.
+"""
+
+
+def function_3(numerator, denominator):
+    return numerator / denominator
+
+
+if __name__ == "__main__":
+    print("This script contains code examples.")
+    print("Example division: ", function_3(10, 0))
